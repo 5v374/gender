39c41625f35f7c56344f2ae995dee5874a0cbcb2
5v374 <sveta04ne@icloud.com>
2025-04-04 18:35:35 +0300

    added photo

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 function revealGender() {   
     const resultText = document.getElementById("result");
     const isBoy = false;
-    resultText.innerText = isBoy ? "У нас будет МАЛЬЧИК!" : "У нас будет ДЕВОЧКА!";
+    resultText.innerText = isBoy ? "У нас будет МАЛЬЧИК!" : "У нас будет ДЕВОЧКА!(вроде)";
     resultText.style.color = isBoy ? "#3f51b5" : "#dd82a4";
     resultText.style.display = "block";
   }m
